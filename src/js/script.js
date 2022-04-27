b41f9efdfdda0c72cec7807bb9f43921751aa466
Fábio Junik <fabiojunik@gmail.com>
2022-04-27 06:21:58 +0100

    Calculadora funcionando

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,30 @@
 const container = document.querySelector('.container');
 const calculator = container.querySelector('table');
 const btnMode = calculator.querySelector('.mode');
+const display = calculator.querySelector('.display');
 
 btnMode.addEventListener('click',changeMode);
+document.querySelectorAll('button').forEach(btn=>{
+    
+    btn.addEventListener('click',()=>{
+    
+        const {value} = display;
+
+        if(btn.classList.contains('btn-clear'))
+            display.value = " ";
+
+        else if(btn.classList.contains('btn-equal'))
+            display.value = eval(value)
+
+        else if(btn.classList.contains('btn-del'))
+            display.value = value.substring(0,value.length -1)
+        
+        else
+            display.value += btn.textContent;
+    })
+    
+})
+
 
 function changeMode(){
     if(container.classList.contains('dark')){
